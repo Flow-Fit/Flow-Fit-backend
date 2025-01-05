5974ce8cb99d927599c120ae692da58f87890aa1
chan000518 <wkdvnddl050701@gmail.com>
2025-01-05 16:46:31 +0900

    feat: 프리즈마, 슈퍼 스트럭트 추가
    프리즈마와 슈퍼 스트럭트 설치 및 에러 처리 추가

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -58,34 +58,34 @@ const errorHandler = (err, req, res, next) => {
     });
   }
 
-//   // Prisma의 Known Request Error 처리
-//   if (err instanceof Prisma.PrismaClientKnownRequestError) {
-//     // 에러 코드에 따라 분기 처리
-//     switch (err.code) {
-//       case "P2002":
-//         // 고유 제약 조건 위반 (중복된 값)
-//         return res.status(ErrorCodes.Conflict.code).json({
-//           message: ErrorCodes.Conflict.message,
-//         });
-//       case "P2025":
-//         // 레코드를 찾을 수 없음
-//         return res.status(ErrorCodes.NotFound.code).json({
-//           message: ErrorCodes.NotFound.message,
-//         });
-//       // 다른 에러 코드에 대한 처리 추가 가능
-//       default:
-//         return res.status(ErrorCodes.BadRequest.code).json({
-//           message: err.message,
-//         });
-//     }
-//   }
+  // Prisma의 Known Request Error 처리
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    // 에러 코드에 따라 분기 처리
+    switch (err.code) {
+      case "P2002":
+        // 고유 제약 조건 위반 (중복된 값)
+        return res.status(ErrorCodes.Conflict.code).json({
+          message: ErrorCodes.Conflict.message,
+        });
+      case "P2025":
+        // 레코드를 찾을 수 없음
+        return res.status(ErrorCodes.NotFound.code).json({
+          message: ErrorCodes.NotFound.message,
+        });
+      // 다른 에러 코드에 대한 처리 추가 가능
+      default:
+        return res.status(ErrorCodes.BadRequest.code).json({
+          message: err.message,
+        });
+    }
+  }
 
-//   // Prisma의 Validation Error 처리
-//   if (err instanceof Prisma.PrismaClientValidationError) {
-//     return res.status(ErrorCodes.BadRequest.code).json({
-//       message: "데이터 유효성 검증 오류가 발생했습니다",
-//     });
-//   }
+  // Prisma의 Validation Error 처리
+  if (err instanceof Prisma.PrismaClientValidationError) {
+    return res.status(ErrorCodes.BadRequest.code).json({
+      message: "데이터 유효성 검증 오류가 발생했습니다",
+    });
+  }
 
   // 예상치 못한 에러의 경우
   return res.status(500).json({
